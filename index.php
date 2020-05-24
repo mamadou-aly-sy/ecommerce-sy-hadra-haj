55e96e52ddb35452bdf7b04952dc77daf8b3ef65
Mamadou Aly Sy <mo.aly.sy@gmail.com>
2020-05-25 05:31:32 +0700

    amelioration du fichier index

--- index.php
+++ index.php
@@ -1,2 +1,19 @@
 <?php
 require_once "database/db.php";
+
+ob_start();
+if (isset($_GET['page'])) {
+    $page = $_GET['page'];
+} else {
+    $page = "acceuil";
+}
+
+if ($page == "contact") {
+    require "pages/contact.php";
+} else {
+    require "pages/acceuil.php";
+}
+
+$content = ob_get_clean();
+
+require "pages/default.php";
